5618be9895d82b8b1ec11310358e0de20f230883
fernalp <almanzafernando@hotmail.com>
2024-11-16 16:22:47 -0500

    Se agrega modelo del experimiento

---
+++ modules/module1/model.py
@@ -0,0 +1,8 @@
+
+class experiment:
+
+    def __init__(self, name, date, type_experiment, results):
+      self.name = name
+      self.date = date
+      self.type_experiment = type_experiment
+      self.results = results
